44872902460d722057c3ba9a98323a958b3af89e
Gamborlhini <nikhil.subhas@gmail.com>
2019-01-05 10:01:44 -0500

    Set Borders

--- ClassTest.js
+++ ClassTest.js
@@ -1,8 +1,7 @@
 var canvas, ctx, x = 0, y = 0, dx = 5, dy = 5;
 var rect, char, collision = false, imageObj = new Image();
 imageObj.src="Capture.JPG";
-// OPTIMIZE: clean the uselessness
-var player = [ /*Eyes*/{height:10, width:5, x:x+20, y:y+10, color:"brown"},/*Mouth*/ {height:5, width:20, x:x+5, y:y+25, color:"brown"}, /*Nose*/{height:5, width:5, x:x+15, y:y+20, color:"brown"}, /*Back Hair*/{height:5, width:15, x:x+40, y:y+10, color:"brown"}, {height:10, width:5, x:x+45, y:y+15, color:"brown"}, {height:10, width:5, x:x+55, y:y+15, color:"brown"}, {height:5, width:5, x:x+50, y:y+25, color:"brown"}, /*Ears*/{height:10, width:5, x:x+50, y:y+15, color:"gold"}, /*Hat*/{height:10, width:5, x:x+55, y:y+15, color:"brown"}, {height:10, width:35, x:x+15, y:y+0, color:"red"}, {height:5, width:50, x:x+5, y:y+5, color:"red"},  /*Face*/{height:25, width:10, x:x+25, y:y+10, color:"gold"},  ];
+
 var rect1 = [{height:10,width:10,x:11,y:11,color:"purple"}];
 var rect2 = [{height:50,width:50,x:51,y:11,color:"purple"}];
 var rect2simple = rect2[0];
@@ -14,7 +13,6 @@ function drawNew() {
   rect = new rectangle(10,10,x,y,"purple");
   rect1render = new rectangle(rect1[0].height,rect1[0].width,rect1[0].x,rect1[0].y,rect1[0].color);
   rect2render = new rectangle(rect2simple.height,rect2simple.width,rect2simple.x,rect2simple.y,rect2simple.color);
-  char = new character(player);
   ctx.fillStyle="white";
 	ctx.fillRect(0,0,1000,500);
   ctx.drawImage(imageObj, x,y,10,10);
@@ -27,7 +25,7 @@ function drawNew() {
 function doKeyDown(a){
     switch(a.keyCode){
     	case 65:/*left*/
-      if (collision==false) {
+      if (collision==false&&x>0) {
         	x=x-dx;
           //player=playerLeft;
       }
@@ -36,7 +34,7 @@ function doKeyDown(a){
       }
       	break;
     	case 68:/*right*/
-      if (collision==false) {
+      if (collision==false&&x<990) {
           x=x+dx
           //player=playerRight;
       }
@@ -45,7 +43,7 @@ function doKeyDown(a){
       }
       	break;
     	case 83:
-      if (collision==false) {
+      if (collision==false&&y<490) {
         y=y+dy;
       }
       else {
@@ -53,7 +51,7 @@ function doKeyDown(a){
       }
         break;
       case 87:
-      if (collision==false) {
+      if (collision==false&&y>0) {
         y=y-dy;
       }
       else {
