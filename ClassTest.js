877cab158d30aa0ecbc5c67163380f9ca74e991c
Gamborlhini <nikhil.subhas@gmail.com>
2019-01-05 16:38:23 -0500

    Literally everything

--- ClassTest.js
+++ ClassTest.js
@@ -1,90 +1,10 @@
-var canvas, ctx, x = 0, y = 0, dx = 5, dy = 20;
-var rect,velocity=0, speedside = 2, gravityspeed=2, velocityup=0, char, groundclass, collision = false,grounded=false, imageObj = new Image();
+var canvas,fireyes, ctx, sx = 0, x = 0, y = 0, dx = 5, dy = 20, direction = "right";
+var rect, shooting = false, velocity=0, speedside = 2, gravityspeed=2, velocityup=0, char, groundclass, collision = false,grounded=false, imageObj = new Image();
 imageObj.src="Capture.JPG";
 var ground = {height:10,width:1000,x:0,y:490,color:"brown"};
 canvas=document.getElementById("canvas");
 ctx=canvas.getContext("2d");
 console.log("canvas established");
-// BUG:  You are always one block underneath the ground. I don't know if this is a groundDetect problem or gravity
-// BUG: You can chain jump
-function gravity() {
-  if (grounded==false&&collision==false) {
-    y+=gravityspeed;
-  }
-}
-function drawNew() {
-  rect = new rectangle(25,25,x,y,"purple");
-  groundclass = new rectangle(ground.height,ground.width,ground.x,ground.y,ground.color);
-  ctx.fillStyle="white";
-	ctx.fillRect(0,0,1000,500);
-  ctx.drawImage(imageObj, x,y,25,25);
-  groundclass.render();
-  detectCollisions();
-  gravity();
-  moveSide();
-  moveUp();
-}
-function detectCollisions() {
-  rect.groundDetect(ground);
-}
-function moveUp() {
-  y-=velocityup;
-}
-function moveSide() {
-  x+=velocity;
-}
-function doKeyUp(a) {
-  switch (a.keyCode) {
-    case 65:
-      velocity=0;
-      break;
-    case 68:
-      velocity=0;
-      break;
-  }
-}
-function doKeyDown(a){
-    switch(a.keyCode){
-    	case 65:/*left*/
-      if (collision==false&&x>0) {
-        	velocity=-speedside;
-          //player=playerLeft;
-      }
-      else{
-        console.log("we got a hit");
-      }
-      	break;
-    	case 68:/*right*/
-      if (collision==false&&x<980) {
-        velocity=speedside;
-          //x=x+dx
-          //player=playerRight;
-      }
-      else {
-        console.log("we got a hit");
-      }
-      	break;
-    	case 83:
-      if (collision==false&&y<480&&grounded==false) {
-      //  y=y+dy;
-      }
-      else {
-        console.log("we got a hit");
-      }
-        break;
-      case 87:
-      if (collision==false&&y>0&&grounded==true) {
-        velocityup=8;
-        setTimeout(function () {
-          velocityup=0;
-        }, 100);
-      }
-      else {
-        console.log("we got a hit");
-      }
-        break;
-  }
-}
 class rectangle {
   constructor(height, width, x, y, color) {
     this.height = height;
@@ -116,7 +36,141 @@ class rectangle {
         grounded=false;
       }
   }
+  shotDetect(enemy){
+    if (this.x<enemy.x+enemy.width&&
+    	this.y<enemy.y+enemy.height&&
+    	this.height+this.y>enemy.y&&
+    	this.x+this.width>enemy.x) {
+      //enemy.dead();
+    }
+  }
 }
+// BUG:  You are always one block underneath the ground. I don't know if this is a groundDetect problem or gravity
+// BUG: You can chain jump
+function gravity() {
+  if (grounded==false&&collision==false) {
+    y+=gravityspeed;
+  }
+}
+function fire() {
+  if (fireyes==true) {
+  var shot = new rectangle(5,5,x+sx,y,"black");
+  shot.render();
+  console.log("shot rendered");
+  if (direction == "left") {
+    shooting = true;
+    setTimeout(function () {
+    shooting = false;
+    shot = "null";
+    sx=0;
+  }, 250);
+    sx-=30;
+    console.log(sx);
+    console.log("speed set");
+  }
+  else if (direction == "right") {
+    shooting = true;
+    setTimeout(function () {
+    shooting = false;
+    shot = "null";
+    sx=0;
+  }, 500);
+    sx+=30;
+    console.log("speed set");
+  }
+  else if (direction = "up") {
+    //Shoot up
+  }
+  else {
+    location.reload(true);
+  }
+  }
+}
+function drawNew() {
+  rect = new rectangle(25,25,x,y,"purple");
+  groundclass = new rectangle(ground.height,ground.width,ground.x,ground.y,ground.color);
+  ctx.fillStyle="white";
+	ctx.fillRect(0,0,1000,500);
+  ctx.drawImage(imageObj, x,y,25,25);
+  groundclass.render();
+  detectCollisions();
+  gravity();
+  moveSide();
+  moveUp();
+  fire();
+}
+function detectCollisions() {
+  rect.groundDetect(ground);
+}
+function moveUp() {
+  y-=velocityup;
+}
+function moveSide() {
+  x+=velocity;
+}
+function doKeyUp(a) {
+  switch (a.keyCode) {
+    case 65:
+      velocity=0;
+      break;
+    case 68:
+      velocity=0;
+      break;
+    case 32:
+      fireyes = false;
+      break;
+  }
+}
+function doKeyDown(a){
+    switch(a.keyCode){
+    	case 65:/*left*/
+      if (collision==false&&x>0) {
+        	velocity=-speedside;
+          direction = "left";
+          //player=playerLeft;
+      }
+      else{
+        console.log("we got a hit");
+      }
+      	break;
+    	case 68:/*right*/
+      if (collision==false&&x<980) {
+        velocity=speedside;
+        direction = "right";
+          //x=x+dx
+          //player=playerRight;
+      }
+      else {
+        console.log("we got a hit");
+      }
+      	break;
+    	case 83:
+      if (collision==false&&y<480&&grounded==false) {
+      //  y=y+dy;
+      }
+      else {
+        console.log("we got a hit");
+      }
+        break;
+      case 87:
+      if (collision==false&&y>0&&grounded==true) {
+        velocityup=8;
+        setTimeout(function () {
+          velocityup=0;
+        }, 100);
+      }
+      else {
+        console.log("we got a hit");
+      }
+        break;
+      case 32:
+      if (shooting == false) {
+        fireyes=true;
+      }
+        break;
+  }
+}
+
 class character{
   constructor(array) {
     this.array = array;
