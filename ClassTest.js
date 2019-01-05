dfefa549e8891753d151048fbe26757ec35cd59e
Gamborlhini <nikhil.subhas@gmail.com>
2019-01-05 15:47:21 -0500

    Update ClassTest.js

--- ClassTest.js
+++ ClassTest.js
@@ -1,9 +1,6 @@
 var canvas, ctx, x = 0, y = 0, dx = 5, dy = 20;
-var rect,velocity=0, speedside = 2, gravityspeed=2, char, groundclass, collision = false,grounded=false, imageObj = new Image();
+var rect,velocity=0, speedside = 2, gravityspeed=2, velocityup=0, char, groundclass, collision = false,grounded=false, imageObj = new Image();
 imageObj.src="Capture.JPG";
-
-var rect1simple = {height:10,width:10,x:11,y:11,color:"purple"};
-var rect2simple = {height:50,width:50,x:51,y:11,color:"purple"};
 var ground = {height:10,width:1000,x:0,y:490,color:"brown"};
 canvas=document.getElementById("canvas");
 ctx=canvas.getContext("2d");
@@ -16,24 +13,22 @@ function gravity() {
   }
 }
 function drawNew() {
-  rect = new rectangle(10,10,x,y,"purple");
+  rect = new rectangle(25,25,x,y,"purple");
   groundclass = new rectangle(ground.height,ground.width,ground.x,ground.y,ground.color);
-  rect1render = new rectangle(rect1simple.height,rect1simple.width,rect1simple.x,rect1simple.y,rect1simple.color);
-  rect2render = new rectangle(rect2simple.height,rect2simple.width,rect2simple.x,rect2simple.y,rect2simple.color);
   ctx.fillStyle="white";
 	ctx.fillRect(0,0,1000,500);
-  ctx.drawImage(imageObj, x,y,10,10);
-  rect1render.render();
-  rect2render.render();
+  ctx.drawImage(imageObj, x,y,25,25);
   groundclass.render();
   detectCollisions();
   gravity();
   moveSide();
+  moveUp();
 }
 function detectCollisions() {
-  rect.collisionDetect(rect1simple);
-  rect.collisionDetect(rect2simple);
   rect.groundDetect(ground);
+}
+function moveUp() {
+  y-=velocityup;
 }
 function moveSide() {
   x+=velocity;
@@ -60,7 +55,7 @@ function doKeyDown(a){
       }
       	break;
     	case 68:/*right*/
-      if (collision==false&&x<990) {
+      if (collision==false&&x<980) {
         velocity=speedside;
           //x=x+dx
           //player=playerRight;
@@ -70,7 +65,7 @@ function doKeyDown(a){
       }
       	break;
     	case 83:
-      if (collision==false&&y<490&&grounded==false) {
+      if (collision==false&&y<480&&grounded==false) {
       //  y=y+dy;
       }
       else {
@@ -78,8 +73,11 @@ function doKeyDown(a){
       }
         break;
       case 87:
-      if (collision==false&&y>0) {
-        y-=dy;
+      if (collision==false&&y>0&&grounded==true) {
+        velocityup=8;
+        setTimeout(function () {
+          velocityup=0;
+        }, 100);
       }
       else {
         console.log("we got a hit");
