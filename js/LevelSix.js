0fc72a0f684523e02608fd08a8750aa53208f007
Gamborlhini <nikhil.subhas@gmail.com>
2020-02-20 12:00:50 -0500

    fixed lvl6

--- js/LevelSix.js
+++ js/LevelSix.js
@@ -21,12 +21,12 @@ var rect, shooting = false,
   imageMortar = new Image(),
   imageMortarshot = new Image(),
   imageEnemy = new Image();
-imageObj.src = "Slime.JPG";
-imagePortal.src = "EndPortal.png";
-imageCoin.src = "Coin.png";
-imageEnemy.src = "Enemy.png";
-imageMortar.src = "Mortar.png";
-imageMortarshot.src = "Mortarshot.png";
+imageObj.src = "../imgs/Slime.JPG";
+imagePortal.src = "../imgs/EndPortal.png";
+imageCoin.src = "../imgs/Coin.png";
+imageEnemy.src = "../imgs/Enemy.png";
+imageMortar.src = "../imgs/Mortar.png";
+imageMortarshot.src = "../imgs/Mortarshot.png";
 var grounds = [];
 var belowgrounds = [];
 var splatsound = document.getElementById("splat");
@@ -97,7 +97,7 @@ function winDetect(thiss, platform) {
 }
 function winRedirect() {
   if (winblock == true) {
-    location.assign("LevelSeven.html");
+    location.assign("../html/LevelSeven.html");
   }
 }
 //Reloading function. Times how fast you can reload.
@@ -593,7 +593,7 @@ function doKeyDown(a) {
       if (collision == false && x > 0) {
         velocityleft = speedleft;
         direction = "left";
-        imageObj.src = "Slime.JPG";
+        imageObj.src = "../imgs/Slime.JPG";
         //player=playerLeft;
       } else {
         console.log("we got a hit");
@@ -604,7 +604,7 @@ function doKeyDown(a) {
       if (collision == false && x < 980) {
         velocityright = speedright;
         direction = "right";
-        imageObj.src = "Slime.JPG";
+        imageObj.src = "../imgs/Slime.JPG";
         //x=x+dx
         //player=playerRight;
       } else {
